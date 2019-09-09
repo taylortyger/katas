10fda6f9e4873f5871f341f628cbd3cbc9af3bac
Taylor Tyger <taylortyger@gmail.com>
2019-09-08 22:54:23 -0600

    convertToLCD returns string LCD respresentation of a single digit (0-9)

---
+++ LCD_digits/digits.js
@@ -0,0 +1,43 @@
+const ZERO = " _ \n" +
+                    "| |\n" +
+                    "|_|";
+
+const ONE =  "  |\n" +
+                    "  |\n" +
+                    "  |";
+
+const TWO =  " _ \n" +
+                    " _|\n" +
+                    "|_ ";
+
+const THREE = " _ \n" +
+                     " _|\n" +
+                     " _|";
+
+const FOUR = "   \n" +
+                    "|_|\n" +
+                    "  |";
+const FIVE = " _ \n" +
+                    "|_ \n" +
+                    " _|";
+
+const SIX =  " _ \n" +
+                    "|_ \n" +
+                    "|_|";
+
+const SEVEN = " _ \n" +
+                     "  |\n" +
+                     "  |";
+
+const EIGHT = " _ \n" +
+                     "|_|\n" +
+                     "|_|";
+
+const NINE =        " _ \n" +
+                    "|_|\n" +
+                    "  |";
+
+module.exports = {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+}
+
--- LCD_digits/displayer.js
+++ LCD_digits/displayer.js
@@ -1,7 +1,21 @@
-function sum(a, b){
-    return a + b;
+const digits = require('./digits');
+
+function convertToLCD(number){
+    switch(number) {
+        case 0: return digits.ZERO;
+        case 1: return digits.ONE;
+        case 2: return digits.TWO;
+        case 3: return digits.THREE;
+        case 4: return digits.FOUR;
+        case 5: return digits.FIVE;
+        case 6: return digits.SIX;
+        case 7: return digits.SEVEN;
+        case 8: return digits.EIGHT;
+        case 9: return digits.NINE;
+        default: throw "Expected single digit 0-9";
+    }
 }
 
 module.exports = {
-    sum: sum
+    convertToLCD: convertToLCD
 }
