8fc295a88d2df101c028c53ba96d697e1efac6ae
Taylor Tyger <taylortyger@gmail.com>
2019-09-15 23:46:17 -0600

    STARTING KATA OVER

--- LCD_digits/cli.js
+++ LCD_digits/cli.js
@@ -2,9 +2,3 @@
 const LCDNumber = require('./digits/LCDNumber').LCDNumber;
 
 const [,, ...args] = process.argv;
-
-if(args) {
-    for(number of args) {
-        new LCDNumber(number).print();
-    }
-}
--- LCD_digits/test/display.js
+++ LCD_digits/test/display.js
@@ -1,40 +1,9 @@
 let chai = require('chai');
 let should = chai.should();
-let displayer = require('../displayer');
-let digits = require('../digits');
-
 describe('Calculator Functions', () => {
     describe('convertToLCD()', () => {
         it('should return a LCD represenation of a single digit', () => {
-            let res = displayer.convertToLCD(0);
-            res.should.be.equal(digits.ZERO);
-
-            res = displayer.convertToLCD(1);
-            res.should.be.equal(digits.ONE);
-
-            res = displayer.convertToLCD(2);
-            res.should.be.equal(digits.TWO);
-
-            res = displayer.convertToLCD(3);
-            res.should.be.equal(digits.THREE);
-
-            res = displayer.convertToLCD(4);
-            res.should.be.equal(digits.FOUR);
-
-            res = displayer.convertToLCD(5);
-            res.should.be.equal(digits.FIVE);
-
-            res = displayer.convertToLCD(6);
-            res.should.be.equal(digits.SIX);
-
-            res = displayer.convertToLCD(7);
-            res.should.be.equal(digits.SEVEN);
-
-            res = displayer.convertToLCD(8);
-            res.should.be.equal(digits.EIGHT);
-
-            res = displayer.convertToLCD(9);
-            res.should.be.equal(digits.NINE);
+            
 
         });
     });
