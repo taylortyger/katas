6eb1e8fab858a962996fc67d4e77ad6a63ad54c1
Taylor Tyger <taylortyger@gmail.com>
2019-09-16 01:49:47 -0600

    Patterns should  be a 3x3 grid

--- LCD_digits/test/display.js
+++ LCD_digits/test/display.js
@@ -1,10 +1,16 @@
 let chai = require('chai');
+let expect = chai.expect;
 let should = chai.should();
-describe('Calculator Functions', () => {
-    describe('convertToLCD()', () => {
-        it('should return a LCD represenation of a single digit', () => {
-            
+let Patterns = require('../Patterns').patterns;
 
-        });
+describe('All patterns', () => {
+    it('should be a 3x3 grid', () => {
+        for (key in patterns) {
+            let pattern = patterns[key];
+            expect(pattern).to.have.lengthOf(3);
+            expect(pattern[0]).to.have.lengthOf(3);
+            expect(pattern[1]).to.have.lengthOf(3);
+            expect(pattern[2]).to.have.lengthOf(3);
+        }
     });
 });
