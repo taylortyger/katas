5f9c47f23523f5224cf5d4a23ef7232afc313e3a
Taylor Tyger <taylortyger@gmail.com>
2019-09-16 13:05:52 -0600

    refactor: split logic into their own functions

--- LCD_digits/LCDNumberConverter.js
+++ LCD_digits/LCDNumberConverter.js
@@ -1,13 +1,14 @@
 let patterns = require('./Patterns').patterns;
 
-const LCDNumberConverter = {
-    
-    convertToLCDString(number) {
-        if(typeof(number) === 'string' && !isNaN(number)) number = parseInt(number);
-        if(typeof(number) !== 'number') number = 0;
-        number = Math.trunc(number);
-        let digits = number.toString().split('').map(digit => parseInt(digit));
-        let lines = ['','',''];
+function cleanInput(number) {
+    if(typeof(number) === 'string' && !isNaN(number)) number = parseInt(number);
+    if(typeof(number) !== 'number') number = 0;
+    number = Math.trunc(number);
+    return number;
+}
+
+function buildLCDString(digits) {
+    let lines = ['','',''];
         for(let i = 0; i < digits.length; i++) {
             let digitPattern = patterns[digits[i]];
             lines[0] += digitPattern[0];
@@ -20,6 +21,18 @@ const LCDNumberConverter = {
             }
         }
         return lines.join('\n');
+}
+
+function getDigitArrayOf(number) {
+    return number.toString().split('').map(digit => parseInt(digit));
+}
+
+const LCDNumberConverter = {
+    
+    convertToLCDString(number) {
+        number = cleanInput(number);
+        let digits = getDigitArrayOf(number);
+        return buildLCDString(digits);
     }
 }
 
